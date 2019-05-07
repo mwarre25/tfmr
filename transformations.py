5478754029279e711a097c1d8501a904a80eabf6
Michael Warren <mwarre25@gmail.com>
2019-05-07 14:36:04 -0400

    moved nltk_pipes to transformations

--- transformations.py
+++ transformations.py
@@ -1,4 +1,6 @@
 import pandas as import pd
+import pdpipe as pdp
+
 import tfmr.lists as lists
 
 
@@ -111,3 +113,34 @@ def string_year_to_datetime(string_list):
             date_list.append(row)
     return date_list
 
+def get_stems(df, column):
+    '''Input string column, output word stems using pdpipe
+    https://github.com/shaypal5/pdpipe
+
+    This function uses pdpipe, an easy to use pandas piping library,
+    to in turn use nltk to convert sentances into a list of word stems using
+    ntltk functions tokenizer, remove stopwords and english snowball stemmer.
+    
+    - TokenizeWords (https://www.nltk.org/api/nltk.tokenize.html),
+    - RemoveStopWords (https://stackoverflow.com/questions/5486337/how-to-remove-stop-words-using-nltk-or-python),
+    - SnowballStemmer/EnglishStemmer (http://www.nltk.org/howto/stem.html)}
+
+    Args:
+        df (dataframe): dataframe of column to be stemmed
+        column (dataframe): column to be stemmed
+    
+    Returns:
+        dataframe: Copied dataframe with new columns for each step (tokenize, remove stop words, stemmer)
+    '''
+    pipeline = pdp.TokenizeWords(columns=[column], drop=False)
+
+
+    pipeline += pdp.RemoveStopwords(language='english',
+                                    columns=[column + '_tok'],
+                                    drop=False)
+    pipeline += pdp.SnowballStem('EnglishStemmer',
+                                columns=[column + '_tok' + '_nostop'],
+                                drop=False)
+    
+    new_df = pipeline(df.copy())
+    return new_df
