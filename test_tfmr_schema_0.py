e69c1c38b2940763945a13af4b4ab492c2f130a3
Warren, Michael <mwarre25@gmail.com>
2019-05-22 11:22:14 -0400

    deletd selector/diverter tbl fns (LTCDGA/OQ repl)

--- test_tfmr_schema_0.py
+++ test_tfmr_schema_0.py
@@ -37,112 +37,154 @@ class TestSchema0(unittest.TestCase):
             test_qryBushing[col] = test_qryBushing[col].astype("int")
 
         pd.testing.assert_frame_equal(test_qryBushing, tfmr.schema_0.blank_qryBushing())
-    def test_blank_qryBushingPowerFactor():
+    def test_blank_qryBushingPowerFactor(self):
         '''Making sure schema doesn't change from this version, for qryBushingPowerFactor
         '''
+        qryBushing_list = [
+            "ID", "TFMRID", "BUSHINGID", "MANUFACTURER", "MODEL",
+            "CONNECTIONTYPE", "BIL", "RATEDVOLTAGE", "RATEDCURRENT",
+            "FACTORYC1PF", "FACTORYC1CAP", "FACTORYC2PF", "FACTORYC2CAP"
+        ]
+
+        test_qryBushing = pd.DataFrame(columns=qryBushing_list)
+
+        # strings
+        for col in [
+                "TFMRID", "BUSHINGID", "MANUFACTURER", "MODEL", "CONNECTIONTYPE"
+        ]:
+            test_qryBushing[col] = test_qryBushing[col].astype("str")
+
+        # floats
+        for col in [
+                "BIL",
+                "RATEDVOLTAGE",
+                "RATEDCURRENT",
+                "FACTORYC1PF",
+                "FACTORYC1CAP",
+                "FACTORYC2PF",
+                "FACTORYC2CAP",
+        ]:
+            test_qryBushing[col] = test_qryBushing[col].astype("float")
+
+        # ints
+        for col in ["ID"]:
+            test_qryBushing[col] = test_qryBushing[col].astype("int")
+
+        pd.testing.assert_frame_equal(test_qryBushing, tfmr.schema_0.blank_qryBushing())
         pass
     
-    def test_blank_qryDGA():
+    def test_blank_qryDGA(self):
         '''Making sure schema doesn't change from this version, for qryDGA
         '''
-        pass
-    
-    def test_blank_qryDiverterDGA():
-        '''Making sure schema doesn't change from this version, for qryDiverterDGA
-        '''
-        pass
+        qryBushing_list = [
+            "ID", "TFMRID", "BUSHINGID", "MANUFACTURER", "MODEL",
+            "CONNECTIONTYPE", "BIL", "RATEDVOLTAGE", "RATEDCURRENT",
+            "FACTORYC1PF", "FACTORYC1CAP", "FACTORYC2PF", "FACTORYC2CAP"
+        ]
 
-    def test_blank_qryDiverterOilQuality():
-        '''Making sure schema doesn't change from this version, for qryDiverterOilQuality
-        '''
+        test_qryBushing = pd.DataFrame(columns=qryBushing_list)
+
+        # strings
+        for col in [
+                "TFMRID", "BUSHINGID", "MANUFACTURER", "MODEL", "CONNECTIONTYPE"
+        ]:
+            test_qryBushing[col] = test_qryBushing[col].astype("str")
+
+        # floats
+        for col in [
+                "BIL",
+                "RATEDVOLTAGE",
+                "RATEDCURRENT",
+                "FACTORYC1PF",
+                "FACTORYC1CAP",
+                "FACTORYC2PF",
+                "FACTORYC2CAP",
+        ]:
+            test_qryBushing[col] = test_qryBushing[col].astype("float")
+
+        # ints
+        for col in ["ID"]:
+            test_qryBushing[col] = test_qryBushing[col].astype("int")
+
+        pd.testing.assert_frame_equal(test_qryBushing, tfmr.schema_0.blank_qryBushing())
         pass
     
-    def test_blank_qryFurans():
+    def test_blank_qryFurans(self):
         '''Making sure schema doesn't change from this version, for qryFurans
         '''
         pass
     
-    def test_blank_qryInsulationPF():
+    def test_blank_qryInsulationPF(self):
         '''Making sure schema doesn't change from this version, for qryInsulationPF
         '''
         pass
     
-    def test_blank_qryLTC():
+    def test_blank_qryLTC(self):
         '''Making sure schema doesn't change from this version, for qryLTC
         '''
         pass
     
-    def test_blank_qryLTCDGA():
+    def test_blank_qryLTCDGA(self):
         '''Making sure schema doesn't change from this version, for qryLTCDGA
         '''
         pass
     
-    def test_blank_qryLTCOilQuality():
+    def test_blank_qryLTCOilQuality(self):
         '''Making sure schema doesn't change from this version, for qryLTCOilQuality
         '''
         pass
     
-    def test_blank_qryLTCTapCount():
+    def test_blank_qryLTCTapCount(self):
         '''Making sure schema doesn't change from this version, for qryLTCTapCount
         '''
         pass
     
-    def test_blank_qryLTCTapPosition():
+    def test_blank_qryLTCTapPosition(self):
         '''Making sure schema doesn't change from this version, for qryLTCTapPosition
         '''
         pass
     
-    def test_blank_qryOilQuality():
+    def test_blank_qryOilQuality(self):
         '''Making sure schema doesn't change from this version, for qryOilQuality
         '''
         pass
     
-    def test_blank_qrySelectorDGA():
-        '''Making sure schema doesn't change from this version, for qrySelectorDGA
-        '''
-        pass
-    
-    def test_blank_qrySelectorOilQuality():
-        '''Making sure schema doesn't change from this version, for qrySelectorOilQuality
-        '''
-        pass
-    
-    def test_blank_qryTfmrs():
+    def test_blank_qryTfmrs(self):
         '''Making sure schema doesn't change from this version, for qryTfmrs
         '''
         pass
     
-    def test_blank_schema_0_results():
+    def test_blank_schema_0_results(self):
         '''Making sure schema doesn't change from this version, for schema_0_results
         '''
         pass
 
-    def test_blank_tblComments():
+    def test_blank_tblComments(self):
         '''Making sure schema doesn't change from this version, for tblComments
         '''
         pass
         
-    def test_blank_tblResultsDetails():
+    def test_blank_tblResultsDetails(self):
         '''Making sure schema doesn't change from this version, for tblResultsDetails
         '''
         pass
     
-    def test_blank_tblResultsOverTime():
+    def test_blank_tblResultsOverTime(self):
         '''Making sure schema doesn't change from this version, for tblResultsOverTime
         '''
         pass
     
-    def test_blank_workbook():
+    def test_blank_workbook(self):
         '''testing test_schema_0_workbook function
         '''
         pass
 
-    def test_schema_0_workbook():
+    def test_schema_0_workbook(self):
         '''testing test_schema_0_workbook function
         '''
         pass
     
-    def test_tblResultsCompare():
+    def test_tblResultsCompare(self):
         '''testing tblResultsCompare function
         '''
         pass
